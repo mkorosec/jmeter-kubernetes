8decadd07019f8a76c4cb720644b34922bb653d2
Matej Korosec <matej.korosec@bearingpoint.com>
2022-12-22 10:12:57 +0100

    race condition resolution

--- simple_grafana_deployment.sh
+++ simple_grafana_deployment.sh
@@ -11,6 +11,7 @@ kubectl wait --for=condition=ready pod --selector='app=influxdb-jmeter' -n jmete
 kubectl exec -ti -n jmeter $(kubectl get po -n jmeter -o jsonpath="{.items[0].metadata.name}" --selector='app=influxdb-jmeter') -- influx -execute 'CREATE DATABASE jmeter'
 echo 'Wait for jmeter-grafana to be ready ..'
 kubectl wait --for=condition=ready pod --selector='app=jmeter-grafana' -n jmeter --timeout=60s
+sleep 1
 kubectl exec -ti -n jmeter $(kubectl get po -n jmeter -o jsonpath="{.items[0].metadata.name}" --selector='app=jmeter-grafana') -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
 echo 'Port forward for grafana: http://localhost:3000'
 kubectl port-forward -n jmeter $(kubectl get po -n jmeter -o jsonpath="{.items[0].metadata.name}" --selector='app=jmeter-grafana') 3000
